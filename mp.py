53c387101b165c11b82d907b0c8d3e32ad84bb0c
ennaed <deannecaingat@gmail.com>
2017-12-19 01:10:08 +0800

    added tokens

--- mp.py
+++ mp.py
@@ -7,6 +7,10 @@ reserved = {
     'while' : 'WHILE',
     'true' : 'TRUE',
     'false' : 'FALSE',
+    'intvar' : 'INTVAR',
+    'floatvar' : 'FLOATVAR',
+    'input' : 'INPUT'
+
 }
 
 
@@ -24,11 +28,23 @@ tokens = [
    # Assignment
    'EQUAL',
 
-
+   # Logical 
+	'OR',
+	'AND',
+	'EQUI',
+	'NEQUI',
+	'LT',
+	'GT',
+	'LTE',
+	'GTE',
 
    # Delimiters
    'LPAREN',
    'RPAREN',
+   'LBRACKET',
+   'RBRACKET',
+   'LBRACE',
+   'RBRACE'
 ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
@@ -38,12 +54,26 @@ t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
 
 t_EQUAL = r'\='
+
+t_OR = r'[|]{2}'
+t_AND = r'[&]{2}'
+t_EQUI = r'[=]{2}'
+t_NEQUI = r'!='
+t_LT = r'<'
+t_GT = r'>'
+t_LTE = r'<='
+t_GTE = r'>='
 
 t_IF = r'if'
 t_ELSE = r'else'
 t_WHILE = r'while'
+t_INPUT = r'in'
 
 
 def t_TRUE(t):
@@ -69,7 +99,16 @@ def t_FLOAT(t):
 
 def t_STRING(t):
 	r'\~.*\~'
-	t.value = t.value.lstrip('$').rstrip('$')
+	t.value = t.value.lstrip('~').rstrip('~')
+	return t
+
+# Variables
+def t_INTVAR(t):
+    r'[i]{2}\~[A-Za-z]+[A-Za-z]*'
+    return t
+
+def t_FLOATVAR(t):
+	r'[f]{2}\~[A-Za-z]+[A-Za-z]*'
 	return t
 
 # Define a rule so we can track line numbers
@@ -90,8 +129,12 @@ lexer = lex.lex()
 
 
 try:
+	
 	filename = input("Enter input file:")
 	i = open(filename, "r")
+	if filename[-3:] != ".md":
+		exit()
+
 	
 except:
 	print(">> Invalid file.")
---
+++ test.md
@@ -0,0 +1,4 @@
+~test~ = 1 + 1
+ii~test = 2
+ff~test = 2.5
+in
